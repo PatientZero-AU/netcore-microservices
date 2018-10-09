17b463d864c2ec224871b5161ba717927fc016a8
Daniel Dekel <daniel_dekel@hotmail.com>
2018-10-09 17:56:45 +1000

    add controller

---
+++ Dan.Service/Controllers/TrainingSessionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dan.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainingSessionController : ControllerBase
+    {
+        
+        public TrainingSessionController()
+        {
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            
+        }
+    }
+}
