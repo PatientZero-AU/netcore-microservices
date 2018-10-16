27dc42910e7ba11434dcdc01d7ebaa58cb016da8
Daniel Dekel <daniel_dekel@hotmail.com>
2018-10-17 08:29:19 +1000

    new controller

---
+++ Dan.Service/Controllers/SomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dan.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        //readonly PaulServiceClient _client;
+        //public SomeController(PaulServiceClient client)
+        //{
+        //    _client = client;
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> Post(CancellationToken cancellationToken)
+        //{
+        //    var response = await _client.Advice_PostAsync();
+        //    return Ok();
+        //}
+
+        //[HttpPut]
+        //public async Task<IActionResult> Put(CancellationToken cancellationToken)
+        //{
+        //    var response = await _client.Advice_PutAsync();
+        //    return Ok();
+        //}
+    }
+}
